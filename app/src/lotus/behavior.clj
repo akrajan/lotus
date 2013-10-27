5b09ec9e69ddb9d21a0f6ed523802d0c00192537
Arun Kumar Rajan <arun@seattlehomepro.com>
2013-10-27 17:19:03 +0530

    Dummy search service

--- app/src/lotus/behavior.clj
+++ app/src/lotus/behavior.clj
@@ -14,9 +14,12 @@
 (defn search-text-fn [old inputs]
   (:search-text inputs))
 
+(defn search-with [search-key]
+  [{msg/type :search-with msg/topic [:search :text] :value search-key}])
 
 (def example-app
   {:version 2
    :transform [[:search-with [:search :text] search-text-fn]]
+   :effect #{[#{[:search :text]} search-with :single-val]}
    :emit [{:in #{[:*]} :fn (app/default-emitter []) :init init-search-box}]})
 
--- app/src/lotus/simulated/start.cljs
+++ app/src/lotus/simulated/start.cljs
@@ -2,6 +2,9 @@
   (:require [io.pedestal.app.render.push.handlers.automatic :as d]
             [lotus.start :as start]
             [lotus.rendering :as rendering]
+            [io.pedestal.app.protocols :as p]
+            [lotus.simulated.services :as services]
+            [io.pedestal.app :as app]
             [goog.Uri]
             ;; This needs to be included somewhere in order for the
             ;; tools to work.
@@ -12,6 +15,8 @@
     (.getParameterValue uri name)))
 
 (defn ^:export main []
-  (start/create-app (if (= "auto" (param "renderer"))
-                      d/data-renderer-config
-                      (rendering/render-config))))
+  (let [app (start/create-app (rendering/render-config))
+        services (services/->SearchService (:app app))]
+    (app/consume-effects (:app app) services/services-fn)
+    (p/start services)
+    app))
